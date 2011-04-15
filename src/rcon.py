19d6b97b6c764341f1b0a9fe18410fc8f46f64e0
Arnim Eijkhoudt <penguin@dhcp.net>
2011-04-15 02:15:11 +0200

    Response uitlezen is gelukt, maar de string unpacken nu nog niet.

--- src/rcon.py
+++ src/rcon.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from xdrlib import *
 from struct import *
 from socket import *
 import socket
@@ -18,10 +19,13 @@ class rconConnection(object):
         self.requestId = 1
         self.connectstate = False
 
+    def unpackhelper(self,fmt,data):
+        size = calcsize(fmt)
+        return unpack(fmt, data[:size]), data[size:]
+
     def command(self,command):
-        self.command = command
-        if connectstate:
-            self.rawsend(self.command)
+        if self.connectstate:
+            self.rawsend(command)
             return self.rawread()
         else:
             return False
@@ -32,48 +36,35 @@ class rconConnection(object):
         self.packet = pack('LL',self.requestId,command)+self.packet
         self.packet = pack('L',len(self.packet))+self.packet
         self.s.send(self.packet)
-
-        ## Debug code (remove later)
-        print "Length :",len(self.packet)
-        print "Content:",self.packet
-        print "Raw    :",repr(self.packet)
-        print "Req. ID:",self.requestId
-        ##
-
         self.requestId += 1
 
     def rawread(self):
         self.packets = []
         self.buffer = ''
-        if len(self.buffer) == 0:
-            chunk = self.s.recv(4)
-            if not chunk:
-                return False
-            length = unpack('L',chunk)[0]
+        self.s.settimeout(1)
+        while True:
+            try:
+                chunk = self.s.recv(4096)
+            except socket.timeout:
+                break
 
-            ## Debug code (remove later)
-            print "Length :",length
-            ##
+            if chunk == "":
+                break
 
-        while (len(self.buffer)<(length-4)):
-            chunk = self.s.recv(4096)
-            self.buffer += chunk
-            print "Reply  :",repr(chunk)
+            responseId = unpack('L',chunk[1:5][::-1])
+            commandResponse = unpack('L',chunk[5:9][::-1])
 
-        content = unpack('LLss',self.buffer)
+            self.buffer = [ responseId,commandResponse ]
 
-        ## Debug code (remove later)
-        print "Content:",repr(content)
-        ##
-
-        return content
+        return self.buffer
 
     def connect(self):
         self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.s.connect((self.host,self.port))
         self.rawsend(self.rcon,'',AUTH)
         response = self.rawread()
-        if response[1] != AUTH_RESPONSE:
+        print response
+        if response[1][0] != 2:
             self.s.close()
             self.connectstate = False
             return False
@@ -85,5 +76,6 @@ if __name__ == "__main__":
     x = rconConnection('145.92.203.100',27115,'byt3m3')
     if x.connect():
         print "Yay!"
+        x.command('status')
     else:
         print "Nay!"
